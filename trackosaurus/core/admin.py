513de0992ea9bfa9fab93e3ce1c98cdabe59af7a
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-04 03:00:44 +0100

    Updated the core admin

--- trackosaurus/core/admin.py
+++ trackosaurus/core/admin.py
@@ -6,6 +6,18 @@ from django.contrib import admin
 from .models import Campaign, StandardEvent, CampaignEvent
 
 # Register your models here.
-admin.register(Campaign)
-admin.register(StandardEvent)
-admin.register(CampaignEvent)
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(StandardEvent)
+class StandardEventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CampaignEvent)
+class CampaignEventAdmin(admin.ModelAdmin):
+    pass
+
+

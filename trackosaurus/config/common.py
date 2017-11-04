640ae941af292c4ad776bf96f1c1715ba07d1b40
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-04 10:50:05 +0100

    Added queryinspect

--- trackosaurus/config/common.py
+++ trackosaurus/config/common.py
@@ -38,7 +38,8 @@ class Common(Configuration):
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'django.middleware.security.SecurityMiddleware'
+        'django.middleware.security.SecurityMiddleware',
+        'qinspect.middleware.QueryInspectMiddleware',
     )
 
     ROOT_URLCONF = 'urls'
@@ -190,7 +191,12 @@ class Common(Configuration):
             'rq.worker': {
                 'handlers': ['rq_console'],
                 'level': 'DEBUG'
-            }
+            },
+            'qinspect': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
         }
     }
 
@@ -235,3 +241,21 @@ class Common(Configuration):
     # admin template so it may interfere with other apps that modify the
     # default admin template. If you're using such an app, simply remove this.
     RQ_SHOW_ADMIN_LINK = True
+
+    # Whether the Query Inspector should do anything (default: False)
+    QUERY_INSPECT_ENABLED = True
+    # Whether to log the stats via Django logging (default: True)
+    QUERY_INSPECT_LOG_STATS = True
+    # Whether to add stats headers (default: True)
+    QUERY_INSPECT_HEADER_STATS = True
+    # Whether to log duplicate queries (default: False)
+    QUERY_INSPECT_LOG_QUERIES = True
+    # Whether to log queries that are above an absolute limit (default: None - disabled)
+    QUERY_INSPECT_ABSOLUTE_LIMIT = 100  # in milliseconds
+    # Whether to log queries that are more than X standard deviations above the mean query time
+    # (default: None - disabled)
+    QUERY_INSPECT_STANDARD_DEVIATION_LIMIT = 2
+    # Whether to include tracebacks in the logs (default: False)
+    QUERY_INSPECT_LOG_TRACEBACKS = True
+    # Project root (a list of directories, see below - default empty)
+    QUERY_INSPECT_TRACEBACK_ROOTS = [BASE_DIR]

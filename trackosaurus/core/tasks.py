424f60c599e0e0a7e7d3a0cff2f4fea8d0dfaf29
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-11 23:09:20 +0100

    Fix subscribing users to a topic

--- trackosaurus/core/tasks.py
+++ trackosaurus/core/tasks.py
@@ -6,13 +6,28 @@ from django.conf import settings
 
 @task
 def register_user_to_topic(token, topic):
+    """
+    https://iid.googleapis.com/iid/v1:batchAdd
+    Content-Type:application/json
+    Authorization:key=API_KEY
+    {
+       "to": "/topics/<YOUR TOPIC NAME HERE>",
+       "registration_tokens": ["<YOUR TOKEN HERE>"]
+    }
+    """
     API_KEY = "AAAAry4xbXQ:APA91bFM6lFskgOsjsPXhdHhdCRA4CafRDw5RNE4RdZjrDeSAgKiTKo0Z9M_spLufLH6rJOUA1xwfnnt0tExqTyig612p3Pu9EiLNcsZj80UHbWA2Dtyu0vyA3jpxblI5vhAgkrQ17dE"
     HEADERS = {
         "Content-Type": "application/json",
         "Authorization": "key={}".format(API_KEY)
     }
-    URL = 'https://iid.googleapis.com/iid/v1/{}/rel/topics/{}'.format(token, topic)
-    r = requests.get(URL, headers=HEADERS)
+    payload = {
+        "to": "/topics/{}".format(topic),
+        "registration_tokens": [token]
+    }
+    URL = 'https://iid.googleapis.com/iid/v1:batchAdd'.format(token, topic)
+    r = requests.post(URL, data=json.dumps(payload), headers=HEADERS)
     if r.status_code == requests.codes.ok:
         RecordedEventToken = apps.get_model('core', 'RecordedEventToken')
         RecordedEventToken.objects.filter(token=token).update(subscribed=True)
+
+

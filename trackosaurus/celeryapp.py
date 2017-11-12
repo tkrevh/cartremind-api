8a3baa729c058fc957d88ff138c996e1a1518dbc
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-12 11:00:05 +0100

    Fixing celery

--- trackosaurus/celeryapp.py
+++ trackosaurus/celeryapp.py
@@ -10,13 +10,18 @@ os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 from configurations import importer
 importer.install()
 
-# from django.conf import settings
+from django.conf import settings
 
 app = Celery('trackosaurus')
-app.conf.update(BROKER_URL=os.environ.get('REDISCLOUD_URL', 'redis://127.0.0.1:6379/1'),
-                CELERY_RESULT_BACKEND=os.environ.get('REDISCLOUD_URL', 'redis://127.0.0.1:6379/1'))
-app.autodiscover_tasks(['trackosaurus.core'], force=True)
+app.conf.update(BROKER_URL=os.environ.get('REDISCLOUD_URL', 'redis://127.0.0.1:6379/1'), CELERY_RESULT_BACKEND=os.environ.get('REDISCLOUD_URL', 'redis://127.0.0.1:6379/1'))
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks(['trackosaurus.core'], force=True)
+app.autodiscover_tasks()
 
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
 
 # class WorkerProc(CeleryProc):
 #     name = 'worker'

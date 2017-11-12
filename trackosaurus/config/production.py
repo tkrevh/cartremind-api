75cc432d524505c3076a6d1222386695a655994b
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-12 20:52:53 +0100

    No need to recreate s3 buckets from code, already fabric setup script takes care of it

--- trackosaurus/config/production.py
+++ trackosaurus/config/production.py
@@ -52,7 +52,7 @@ class Production(Common):
     AWS_SECRET_ACCESS_KEY = values.Value('DJANGO_AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = values.Value('DJANGO_AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = values.Value('public-read')
-    AWS_AUTO_CREATE_BUCKET = True
+    AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = False
     MEDIA_URL = 'https://s3.amazonaws.com/{}/'.format(AWS_STORAGE_BUCKET_NAME)
     AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()

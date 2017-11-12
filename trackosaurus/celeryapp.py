1205cac129812ba081dd2ced480360b656a86a1b
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-12 01:44:11 +0100

    Experimenting with celery init

--- trackosaurus/celeryapp.py
+++ trackosaurus/celeryapp.py
@@ -5,9 +5,9 @@ from celery import Celery
 # from hirefire.procs.celery import CeleryProc
 from django.conf import settings
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
-# os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trackosaurus.config.production')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackosaurus.config")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trackosaurus.config.production')
 
 from configurations import importer
 importer.install()
@@ -15,7 +15,7 @@ importer.install()
 app = Celery('trackosaurus')
 app.conf.update(BROKER_URL=os.environ.get('REDISCLOUD_URL', 'redis://127.0.0.1:6379/1'),
                 CELERY_RESULT_BACKEND=os.environ.get('REDISCLOUD_URL', 'redis://127.0.0.1:6379/1'))
-app.autodiscover_tasks(['core'])
+app.autodiscover_tasks()
 
 
 # class WorkerProc(CeleryProc):

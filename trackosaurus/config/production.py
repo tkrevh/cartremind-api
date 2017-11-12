ebdb0c4f8cb931f9232f6f5ffdad09bc7593b951
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-12 23:06:51 +0100

    Fix MEDIA_URL in settings

--- trackosaurus/config/production.py
+++ trackosaurus/config/production.py
@@ -54,11 +54,11 @@ class Production(Common):
     AWS_DEFAULT_ACL = values.Value('public-read')
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = False
-    MEDIA_URL = 'https://s3.amazonaws.com/{}/'.format(AWS_STORAGE_BUCKET_NAME)
+    MEDIA_URL = 'https://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
     AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
     AWS_S3_REGION_NAME = values.Value('DJANGO_AWS_S3_REGION_NAME')
     AWS_S3_HOST = values.Value('DJANGO_AWS_S3_HOST')
-    
+
 
     # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
     # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day

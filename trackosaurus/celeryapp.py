2ac1e0094c003c7c3b55228e52aa974090d07514
Tadej Krevh <tadej.krevh@gmail.com>
2017-11-12 10:33:36 +0100

    Celery fixing

--- trackosaurus/celeryapp.py
+++ trackosaurus/celeryapp.py
@@ -1,14 +1,14 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 # from hirefire.procs.celery import CeleryProc
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackosaurus.config")
-#os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackosaurus.config.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackosaurus.config")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackosaurus.config.production")
 
-# from configurations import importer
-# importer.install()
+from configurations import importer
+importer.install()
 
 # from django.conf import settings
 
